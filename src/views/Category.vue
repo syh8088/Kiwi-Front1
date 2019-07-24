0094a044dce0ad5385f1de3fd43dbc4a255c213b
syh80881 <syh8088@gmail.com>
2019-07-24 17:30:54 +0900

    test
    test

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -159,3 +159,155 @@
         opacity: 0;
     }
 </style>
+
+<!--
+
+<template>
+    <div>
+        <base-header type="gradient-success" class="pb-6 pb-8 pt-5 pt-md-8">
+            &lt;!&ndash; Card stats &ndash;&gt;
+            <div class="row">
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Total traffic"
+                                type="gradient-red"
+                                sub-title="350,897"
+                                icon="ni ni-active-40"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 3.48%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Total traffic"
+                                type="gradient-orange"
+                                sub-title="2,356"
+                                icon="ni ni-chart-pie-35"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 12.18%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Sales"
+                                type="gradient-green"
+                                sub-title="924"
+                                icon="ni ni-money-coins"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-danger mr-2"><i class="fa fa-arrow-down"></i> 5.72%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+
+                </div>
+                <div class="col-xl-3 col-lg-6">
+                    <stats-card title="Performance"
+                                type="gradient-info"
+                                sub-title="49,65%"
+                                icon="ni ni-chart-bar-32"
+                                class="mb-4 mb-xl-0"
+                    >
+
+                        <template slot="footer">
+                            <span class="text-success mr-2"><i class="fa fa-arrow-up"></i> 54.8%</span>
+                            <span class="text-nowrap">Since last month</span>
+                        </template>
+                    </stats-card>
+                </div>
+            </div>
+        </base-header>
+
+        <div class="container-fluid mt&#45;&#45;7">
+            <div class="row">
+                <div class="col">
+                    <div class="card shadow">
+                        <div class="card-header bg-transparent">
+                            <h3 class="mb-0">Category</h3>
+                        </div>
+                        <div class="card-body">
+                            <div class="row icon-examples">
+                                <template>
+                                    <div class="justify-content-between row">
+                                        <nested-test class="col-8" v-model="elements" />
+                                        <raw-displayer class="col-4" :title="'Vuex Store'" :value="elements" />
+                                    </div>
+                                </template>
+
+
+                                &lt;!&ndash; <div class="col-lg-3 col-md-6" v-for="(icon, index) in icons" :key="icon.name + index">
+                                     <button type="button"
+                                             v-b-tooltip.hover.top
+                                             :title="icon.name"
+                                             v-clipboard:copy="icon.name"
+                                             v-clipboard:success="onCopy"
+                                             class="btn-icon-clipboard" data-clipboard-text="air-baloon">
+                                         <div>
+                                             <i :class="icon.name"></i>
+                                             <span>{{icon.name.substring(6)}}</span>
+                                         </div>
+                                     </button>
+                                 </div>&ndash;&gt;
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+    </div>
+</template>
+<script>
+    import NestedTest from "./nested/nested-test.vue";
+    import rawDisplayer from "./infra/raw-displayer.vue";
+    export default {
+        name: "nested-with-vmodel",
+        display: "Nested (v-model & vuex)",
+        order: 16,
+        components: {
+            NestedTest,
+            rawDisplayer
+        },
+        computed: {
+            elements: {
+            }
+        },
+        methods: {}
+    };
+</script>
+<style scoped>
+    .title {
+        margin-bottom: 2em;
+    }
+    .bigger-area > span {
+        min-height: 90vh;
+        display: block;
+    }
+    .drag-item {
+        padding: 15px 10px;
+        background-color: whitesmoke;
+        min-width: 30vw;
+        max-width: 90vw;
+        margin: 0 auto 10px;
+        cursor: -moz-grab;
+        cursor: -webkit-grab;
+        cursor: grab;
+        transition: transform 0.25s cubic-bezier(0.02, 1.01, 0.94, 1.01);
+        will-change: transform;
+    }
+    .list-complete-enter,
+    .list-complete-leave-active {
+        opacity: 0;
+    }
+</style>
+
+-->
