61f7b2bb3b08feb12bfd5797e098c17504f794b8
syh80881 <syh8088@gmail.com>
2019-07-28 20:39:55 +0900

    카테고리 삭제
    카테고리 삭제

--- src/views/Category.vue
+++ src/views/Category.vue
@@ -74,7 +74,7 @@
                             <div class="row icon-examples">
                                 <div class="col-3">
                                     <Tree :data="categoriesData" draggable="draggable" cross-tree="cross-tree">
-                                        <div :data-no="data.categoryNo" :data-name="data.name" :data-hierarchicalOrder="data.hierarchicalOrder" @dblclick="dblclick" slot-scope="{data, store}">
+                                        <div :data-_id="data._id" :data-no="data.categoryNo" :data-name="data.name" :data-hierarchicalOrder="data.hierarchicalOrder" @click="click" @dblclick="dblclick" slot-scope="{data, store}">
                                             <template v-if="!data.isDragPlaceHolder">
                                                 <b v-if="data.children &amp;&amp; data.children.length" @click="store.toggleOpen(data)">
                                                     {{data.open ? '-' : '+'}}&nbsp;
@@ -85,6 +85,7 @@
                                     </Tree>
                                     <div class="mt-2">
                                         <button class="btn btn-primary" @click="add">Add</button>
+                                        <button class="btn btn-primary" @click="remove">Remove</button>
                                         <button class="btn btn-primary" @click="save">저장</button>
                                     </div>
                                 </div>
@@ -95,9 +96,10 @@
             </div>
         </div>
 
-        <div id="regist-frame" style="display: none; position: absolute; width: 400px; background-color: white; border: 1px solid black; padding: 10px; z-index: 2; top: 50%; left: 50%; margin-top: 86.5px; margin-left: -211px;">
+        <div id="resist-frame" style="display: none; position: absolute; width: 400px; background-color: white; border: 1px solid black; padding: 10px; z-index: 2; top: 50%; left: 50%; margin-top: 86.5px; margin-left: -211px;">
             <!--<div style="float:right;width:10px;margin-top:-5px"><a onclick="">×</a></div>-->
             <form id="save_form" name="save_form" onsubmit="return false">
+                <input type="hidden" id="_id" name="_id" value="">
                 <input type="hidden" id="categoryNo" name="categoryNo" value="">
                 <input type="hidden" id="hierarchicalOrder" name="hierarchicalOrder" value="">
                 <table width="100%" align="center" cellpadding="5" cellspacing="0" border="1" bordercolor="#dedede" class="table1" style="table-layout:fixed">
@@ -135,6 +137,9 @@
     },
     data() {
         return {
+            totalCount: 0,
+            seletedId: null,
+            saveCategoryRequests: [],
             categoriesData: [
     /*                {categoryNo: 1, name: 'node 1', displayOrder: 1, hierarchicalOrder: 1},
                 {categoryNo: 2, name: 'node 2', displayOrder: 2, hierarchicalOrder: 1},
@@ -146,7 +151,7 @@
                 {categoryNo: 8, name: 'node 8', displayOrder: 8, hierarchicalOrder: 2},*/
 
 
-                {categoryNo: 3, name: 'node 3', displayOrder: 3, hierarchicalOrder: 1},
+         /*       {categoryNo: 3, name: 'node 3', displayOrder: 3, hierarchicalOrder: 1},
                 {categoryNo: 2, name: 'node 2', displayOrder: 2, hierarchicalOrder: 1, children: [
                         {categoryNo: 9, name: 'node 2_2', displayOrder: 2},
                         {categoryNo: 10, name: 'node 2_1', displayOrder: 1, children: [
@@ -161,7 +166,7 @@
                 {categoryNo: 5, name: 'node 5', displayOrder: 5, hierarchicalOrder: 2},
                 {categoryNo: 4, name: 'node 4', displayOrder: 4, hierarchicalOrder: 2},
                 {categoryNo: 7, name: 'node 7', displayOrder: 7, hierarchicalOrder: 2},
-                {categoryNo: 6, name: 'node 6', displayOrder: 6, hierarchicalOrder: 2},
+                {categoryNo: 6, name: 'node 6', displayOrder: 6, hierarchicalOrder: 2},*/
             ]
         };
     },
@@ -176,75 +181,104 @@
         this.$api.getCategories().then(response => {
             console.log(response);
             if(response.status === 200 || response.status === 204) {
-                   this.categoriesData = response.data.categoryResponses;
-
-
+                this.categoriesData = response.data.categoryResponses;
             }
 
         }).catch(e => {
             console.log(e);
         });
 
-
-
         this.categoriesDataSort(this.categoriesData);
     },
     methods: {
-        // add child to tree2
         add() {
-            console.log(this.categoriesData);
+            this.setTotalCount(this.categoriesData);
+            this.categoriesData.push({categoryNo: 0, name: '새로운 카테고리_' + parseInt(this.totalCount + 1)  + "번째"});
 
-            this.categoriesData.push({text: 'chi1123123123ld'})
+        },
+        remove() {
+            if(!this.seletedId) alert("카테고리를 선택해주세요.");
+            this.removeCategory(this.categoriesData);
+            this.seletedId = null;
+            console.log(this.categoriesData);
+        },
+        removeCategory(categoriesData) {
+            let k = 0;
+            for (; k < categoriesData.length ; k++ ) {
+
+                if(categoriesData[k]._id === this.seletedId) {
+                    categoriesData.splice(k, 1);
+                    return true;
+                }
+
+                if(categoriesData[k].children !== undefined && categoriesData[k].children.length > 0) this.removeCategory(categoriesData[k].children);
+
+            }
+        },
+        setDisplayOrder(categoriesData) {
+
+            let k = 0;
+            for (; k < categoriesData.length ; k++ ) {
+                categoriesData[k].displayOrder = parseInt(k + 1);
+                if(categoriesData[k].children !== undefined && categoriesData[k].children.length > 0) this.setDisplayOrder(categoriesData[k].children);
+            }
+        },
+        setTotalCount(categoriesData, rootCategory = true) {
+
+            if(rootCategory) this.totalCount = 0;
+
+            let k = 0;
+            for (; k < categoriesData.length ; k++ ) {
+                this.totalCount ++;
+                if(categoriesData[k].children !== undefined && categoriesData[k].children.length > 0) this.setTotalCount(categoriesData[k].children, false);
+            }
+
+        },
+        click(el) {
+            if(this.seletedId) {
+                document.querySelector("#" + this.seletedId).firstElementChild.style.background= ""
+            }
+
+            this.seletedId = el.currentTarget.getAttribute("data-_id");
+            document.querySelector("#" + this.seletedId).firstElementChild.style.background= "#d6e3f1"
         },
         dblclick(el) {
             this.$common.blind().draw();
 
+            let _id = el.currentTarget.getAttribute("data-_id");
             let categoryNo = el.currentTarget.getAttribute("data-no");
             let categoryName = el.currentTarget.getAttribute("data-name");
             let hierarchicalOrder = el.currentTarget.getAttribute("data-hierarchicalOrder");
 
+            document.getElementById("_id").value = _id;
             document.getElementById("categoryNo").value = categoryNo;
             document.getElementById("name").value = categoryName;
             document.getElementById("hierarchicalOrder").value = hierarchicalOrder;
 
-            document.getElementById("regist-frame").style.display = 'block';
+            document.getElementById("resist-frame").style.display = 'block';
 
         },
         nameModify() {
+            let _id = document.getElementById("_id").value;
             let categoryNo = document.getElementById("categoryNo").value;
             let categoryName = document.getElementById("name").value;
             let hierarchicalOrder = document.getElementById("hierarchicalOrder").value;
 
-            let k = 0;
-            for (; k < hierarchicalOrder ; k++ ) {
-                //let array = this.categoriesData[].chie
-              //  test();
-            }
-
-       /*     this.categoriesData.forEach((category, index) => {
-                if(category.categoryNo === parseInt(categoryNo)) {
-                    this.categoriesData[index].name = categoryName;
-                }
-            });
-*/
-
-            this.changeCategoryName(this.categoriesData, categoryNo, categoryName);
-
-            console.log(this.categoriesData);
+            this.changeCategoryName(this.categoriesData, _id, categoryNo, categoryName);
             this.close();
         },
-        changeCategoryName(targetArray, targetCategoryNo, targetCategoryName) {
+        changeCategoryName(targetArray, target_id, targetCategoryNo, targetCategoryName) {
             targetArray.forEach((category, index) => {
-                if(category.categoryNo === parseInt(targetCategoryNo)) {
+                if(category._id === target_id) {
                     targetArray[index].name = targetCategoryName;
                     return true;
                 }
-                if(targetArray[index].children.length > 0) this.changeCategoryName(targetArray[index].children, targetCategoryNo, targetCategoryName);
+                if(targetArray[index].children !== undefined && targetArray[index].children.length > 0) this.changeCategoryName(targetArray[index].children, target_id, targetCategoryNo, targetCategoryName);
             });
         },
         close() {
             this.$common.blind().remove();
-            document.getElementById("regist-frame").style.display = 'none';
+            document.getElementById("resist-frame").style.display = 'none';
         },
         categoriesDataSort(targetArray) {
             this.categoriesDataSortByDisplayOrder(targetArray);
@@ -262,10 +296,34 @@
                 return a.displayOrder < b.displayOrder ? -1 : a.displayOrder > b.displayOrder ? 1 : 0;
             });
         },
+        createSaveCategoryRequests(categoriesData, saveCategoryRequests) {
+
+            let k = 0;
+            for (; k < categoriesData.length ; k++ ) {
+
+                saveCategoryRequests[k] = {
+                    categoryNo: categoriesData[k].categoryNo,
+                    name: categoriesData[k].name,
+                    displayOrder: categoriesData[k].displayOrder
+                };
+
+                if(categoriesData[k].children !== undefined && categoriesData[k].children.length > 0) {
+                    saveCategoryRequests[k].children = [];
+                    this.createSaveCategoryRequests(categoriesData[k].children, saveCategoryRequests[k].children);
+                }
+            }
+
+        },
         save() {
 
+            this.saveCategoryRequests = [];
+
+            this.setDisplayOrder(this.categoriesData);
+            this.createSaveCategoryRequests(this.categoriesData, this.saveCategoryRequests);
+            console.log(this.saveCategoryRequests);
+
             let data = {
-                'categoryRequests': this.categoriesData
+                'categoryRequests': this.saveCategoryRequests
             };
 
             this.$api.saveCategories(data).then(response => {
