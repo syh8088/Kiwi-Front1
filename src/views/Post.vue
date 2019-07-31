a3bf7c74165e9f03644ba2b7980917c4e39177bb
syh80881 <syh8088@gmail.com>
2019-07-31 13:38:30 +0900

    post view
    post view

--- src/views/Post.vue
+++ src/views/Post.vue
@@ -63,6 +63,59 @@
             </div>
         </base-header>
 
+
+        <div class="container-fluid mt--7">
+            <card shadow type="secondary">
+                <div slot="header" class="bg-white border-0">
+                    <div class="row align-items-center">
+                        <div class="col-8">
+                            <h3 class="mb-0">My account</h3>
+                        </div>
+                        <div class="col-4 text-right">
+                            <a href="#!" class="btn btn-sm btn-primary">Settings</a>
+                        </div>
+                    </div>
+                </div>
+                <template>
+                    <form @submit.prevent>
+                        <h6 class="heading-small text-muted mb-4">User information</h6>
+                        <div class="pl-lg-4">
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <base-input alternative=""
+                                                label="Username"
+                                                placeholder="Username"
+                                                input-classes="form-control-alternative"
+                                    />
+                                </div>
+                                <div class="col-lg-6">
+                                    <base-input alternative=""
+                                                label="Email address"
+                                                placeholder="jesse@example.com"
+                                                input-classes="form-control-alternative"
+                                                v-model="model.username"
+                                    />
+                                </div>
+                            </div>
+
+                        </div>
+
+                        <hr class="my-4" />
+                        <!-- Description -->
+                        <h6 class="heading-small text-muted mb-4">About me</h6>
+                        <div class="pl-lg-4">
+                            <div class="form-group">
+                                <base-input alternative=""
+                                            label="About Me">
+                                    <textarea rows="4" class="form-control form-control-alternative" placeholder="A few words about you ...">A beautiful Dashboard for Bootstrap 4. It is Free and Open Source.</textarea>
+                                </base-input>
+                            </div>
+                        </div>
+                    </form>
+                </template>
+            </card>
+        </div>
+
         <div class="container-fluid mt--7">
             <div class="row mt-5">
                 <div class="col">
@@ -79,6 +132,21 @@
         name: 'tables',
         components: {
             ProjectsTable
+        },
+        data() {
+            return {
+                model: {
+                    username: '',
+                    email: '',
+                    firstName: '',
+                    lastName: '',
+                    address: '',
+                    city: '',
+                    country: '',
+                    zipCode: '',
+                    about: '',
+                }
+            }
         }
     };
 </script>
